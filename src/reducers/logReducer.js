ed8aa540f6295bf08596704c10c23fcfd9238cb7
mwakisaghu <harrisonchambela2016@gmail.com>
2022-02-28 12:08:50 +0300

    This adds logReducer and fetch the logs from the json server

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null,
+};
+
+/* eslint-disable */
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
