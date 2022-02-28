05843d99635cc1a6b50bf4358a8b8ee39ce1f796
mwakisaghu <harrisonchambela2016@gmail.com>
2022-02-28 12:38:54 +0300

    This adds actions and fetches logs via actions instead of directly from the logs components

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,3 +1,5 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
+
 const initialState = {
   logs: null,
   current: null,
@@ -8,6 +10,23 @@ const initialState = {
 /* eslint-disable */
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOGS_ERROR:
+      console.log(action.payload);
+      return {
+        ...state,
+        payload: action.payload,
+      };
     default: {
       return state;
     }
